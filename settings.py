611ebb5221d6fe99a6350b6e343a34b81af81bb5
Daniil185-gif <petrikoff.daniil@gmail.com>
2025-12-05 15:04:51 +0300

    update: Обновил настройки карты

--- settings.py
+++ settings.py
@@ -1,7 +1,9 @@
+# settings.py
 WIDTH = 800
 HEIGHT = 600
 FPS = 60
 TANK_SPEED = 3
-BULLET_SPEED = 12
-TANK_HP = 100
-BULET_DAMAGE = 34
+BULLET_SPEED = 7
+
+# Новая настройка — тема карты!
+CURRENT_MAP_THEME = "summer"  # "summer", "winter", "desert"
